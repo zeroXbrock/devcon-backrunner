c70472b7e23c257333d67a20ba7d0301a371bc36
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2022-10-14 11:05:17 -0500

    ugh

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,17 @@ async function main() {
     const provider = new providers.WebSocketProvider("ws://192.168.0.42:7777")
 
     provider.on("pending", async (pendingTxHash) => {
-        console.log(pendingTxHash)
+        // console.log(pendingTxHash)
         const tx = await provider.getTransaction(pendingTxHash)
-        if (tx && tx.to && tx.to === contracts.SwapRouter02.address) {
+        if (tx && tx.to && tx.to === contracts.UniSwapRouter02.address) {
             if (tx.data.substring(2, 10) === "1f0464d1") {
                 // multicall detected
                 console.log("multicall!!!")
+                const multicallData = utils.defaultAbiCoder.decode([
+                    "uint256 deadline",
+                    "bytes[] data"
+                ], tx.data.substring(10))
+                console.log("multicall data", multicallData)
             }
             
         }
